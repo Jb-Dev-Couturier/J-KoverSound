bb6350f04b8bcfee8bd318ee55022158ff4298cf
JB-Dev <jibaoke@gmail.com>
2022-09-27 13:33:33 +0200

    rapid Api fetch Music Shazam

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,22 @@
+import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        '5184167115mshee68cce67fd28e5p12e42cjsn5c019554b72a'
+      );
+      return headers;
+    },
+  }),
+  endpoints:(builder)=>({
+    getTopCharts: builder.query({query:()=>'/charts/world'})
+  })
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi
