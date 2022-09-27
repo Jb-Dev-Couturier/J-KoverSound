1376bd6408dfc8bccad7fb2c8a009a85f4a38c30
JB-Dev <jibaoke@gmail.com>
2022-09-27 19:10:26 +0200

    sidebar + hamburger menu

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -5,17 +5,24 @@ import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 const SongCard = ({ song, isPlaying, activeSong,data, i }) => {
+  const dispatch = useDispatch()
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({song,data,i}))
+    dispatch(playPause(true))
+  };
 
-  const handlePauseClick = () => {};
-  const handlePlayClick = () => {};
+
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer ">
       <div className="relative w-full h-56 group">
         <div
           className={`absolute inset-0 justify-center items-center bg-black bg-opacity-70 group-hover:flex ${
             activeSong?.title === song.title
-              ? 'flex bg-black bg-opacity-50'
-              : 'hidden'
+              ? 'flex bg-black bg-opacity-50 border border-lime-500'
+              : 'hidden border border-lime-500'
           }`}
         >
           <PlayPause
@@ -29,10 +36,10 @@ const SongCard = ({ song, isPlaying, activeSong,data, i }) => {
         <img src={song.images?.coverart} alt="song_img" />
       </div>
       <div className="mt-4 flex flex-col">
-        <p className="font-semibold text-lg text-white truncate">
+        <p className="font-semibold text-lg text-lime-100 truncate">
           <Link to={`/songs/${song?.key}`}>{song.title}</Link>
         </p>
-        <p className="text-sm truncate text-gray-400 mt-1">
+        <p className="text-sm truncate text-lime-300 mt-1">
           <Link
             to={
               song.artists
