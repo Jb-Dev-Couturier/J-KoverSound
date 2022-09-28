978228b154aa3d3a5784f42b96cefc9ba7a59789
JB-Dev <jibaoke@gmail.com>
2022-09-28 18:44:46 +0200

    details page

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,32 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
+import { useGetSongDetailsQuery } from '../redux/services/shazamCore';
+
+const SongDetails = () => {
+  const dispatch = useDispatch();
+  const { songid } = useParams();
+  const { ActiveSong, isPlaying } = useSelector((state) => state.player);
+
+  const { data: songData, isFetching: isFetechingSongDetails } =
+    useGetSongDetailsQuery({songid})
+
+  return (
+  <div className='flex flex-col'>
+    <DetailsHeader artistId={''} songData={songData}/>
+    <div className="mb-10">
+        <h2 className='text-lime-100 text-3xl font-bold'>Paroles :</h2>
+        <div className="mt-5">
+            {songData?.sections[1].type === 'LYRICS'?
+            songData?.sections[1].text.map((line,i)=>(
+                <p className="text-lime-200 text-base my-1">{line}</p>
+            )):<p>Désolé pas de Paroles Trouvés</p>}
+        </div>
+    </div>
+  </div>
+  
+  )
+};
 
 export default SongDetails;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,11 +12,12 @@ export const shazamCoreApi = createApi({
       return headers;
     },
   }),
-  endpoints:(builder)=>({
-    getTopCharts: builder.query({query:()=>'/charts/world'})
-  })
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    }),
+  }),
 });
 
-export const {
-    useGetTopChartsQuery,
-} = shazamCoreApi
+export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi;
