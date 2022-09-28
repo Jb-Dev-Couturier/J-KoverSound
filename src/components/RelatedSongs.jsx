f973d946c3919935e02db006b401e0001a396967
JB-Dev <jibaoke@gmail.com>
2022-09-28 19:51:33 +0200

    song detail page completed

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,5 +1,28 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
+import  SongBar  from './SongBar';
+
+const RelatedSongs = ({
+  data,
+  isPlaying,
+  activeSong,
+  handlePause,
+  handlePlay,
+  artistId,
+}) => (
+  <div className="flex flex-col">
+    <h1 className="font-bold text-3xl text-lime-200">Chansons liées :</h1>
+    <div className="mt-6 w-full flex flex-col">
+      {data?.map((song, i) => (
+        <SongBar key={`${song.key}-${artistId}`} 
+        song={song}
+        i={i}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePauseClick={handlePause}
+        handlePlayClick={handlePlay}
+        />
+      ))}
+    </div>
+  </div>
 );
 
 export default RelatedSongs;
--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -2,31 +2,59 @@ import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
-import { useGetSongDetailsQuery } from '../redux/services/shazamCore';
+import {
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} from '../redux/services/shazamCore';
 
 const SongDetails = () => {
   const dispatch = useDispatch();
   const { songid } = useParams();
-  const { ActiveSong, isPlaying } = useSelector((state) => state.player);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+  const handlePlayClick = (song,i) => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
 
-  const { data: songData, isFetching: isFetechingSongDetails } =
-    useGetSongDetailsQuery({songid})
+  const { data: songData, isFetching: isFetchingSongDetails } =
+    useGetSongDetailsQuery({ songid });
+  const {
+    data: data,
+    isFetching: isFetchingRelatedSongs,
+    error,
+  } = useGetSongRelatedQuery({ songid });
+
+  if (isFetchingRelatedSongs || isFetchingSongDetails)
+    return <Loader title={'Recherche details en cours...'} />;
+  if (error) return <Error />;
 
   return (
-  <div className='flex flex-col'>
-    <DetailsHeader artistId={''} songData={songData}/>
-    <div className="mb-10">
-        <h2 className='text-lime-100 text-3xl font-bold'>Paroles :</h2>
+    <div className="flex flex-col">
+      <DetailsHeader artistId={''} songData={songData} />
+      <div className="mb-10">
+        <h2 className="text-lime-100 text-3xl font-bold">Paroles :</h2>
         <div className="mt-5">
-            {songData?.sections[1].type === 'LYRICS'?
-            songData?.sections[1].text.map((line,i)=>(
-                <p className="text-lime-200 text-base my-1">{line}</p>
-            )):<p>Désolé pas de Paroles Trouvés</p>}
+          {songData?.sections[1].type === 'LYRICS' ? (
+            songData?.sections[1].text.map((line, i) => (
+              <p className="text-lime-200 text-base my-1">{line}</p>
+            ))
+          ) : (
+            <p>Désolé pas de Paroles Trouvés</p>
+          )}
         </div>
+      </div>
+      <RelatedSongs
+        data={data}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePause={handlePauseClick}
+        handlePlay={handlePlayClick}
+      />
     </div>
-  </div>
-  
-  )
+  );
 };
 
 export default SongDetails;
