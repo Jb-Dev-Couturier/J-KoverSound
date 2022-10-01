87a91f5eb02e12b604ef90a32b2ef8e33d6a4c8f
JB-Dev <jibaoke@gmail.com>
2022-10-01 11:35:38 +0200

    search bar et search teerms

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,42 @@
-const Searchbar = () => (
-  <div>Loader</div>
-);
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { FiSearch } from 'react-icons/fi';
+
+const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate(`/search/${searchTerm}`);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      autoComplete="off"
+      className="p-2 text-lime-100 focus-within:text-lime-500"
+    >
+      <label htmlFor="search-field" className="sr-only">
+        Rechercher tous les fichiers
+      </label>
+      <div className="flex flex-row justify-start items-center">
+        <FiSearch aria-hidden="true" className="w-5 h-5 ml-4" />
+        <input
+          name="search-field"
+          autoComplete="off"
+          id="search-field"
+          className="flex-1 bg-transparent border-none placeholder-lime-200 focus-within:placeholder-lime-800 outline-none text-base text-white p-4"
+          placeholder="Recherche"
+          type="search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </form>
+  );
+};
 
 export default Searchbar;
